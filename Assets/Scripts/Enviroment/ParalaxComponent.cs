51dc7744c1b8cc9135d2bd7bcb654d1754c9496b
filip-matijevic <filip.matijevic@smartlumies.com>
2020-06-30 17:41:57 +0200

    Added paralax assets to the game

---
+++ Assets/Scripts/Enviroment/ParalaxComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxComponent : MonoBehaviour
+{
+    [SerializeField] private float _paralaxDampner = 0.1f;
+    [SerializeField] private Transform _player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = Vector3.right * _player.position.x * _paralaxDampner;
+    }
+}
